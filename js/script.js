0342f777d728ec634335b8625aa0df7ebac9bb86
WO-Nagano Tamawo <nagano.tamawo77@gmail.com>
2022-06-29 17:45:35 +0900

    VRMファイルをドラッグ＆ドロップできるように

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 let lastTime,
-    angle,
-    vrm;
+    angle = 0,
+    vrm,
+    isLoaded = false,
+    dropZone = document.getElementById('drop-zone');
+
+dropZone.addEventListener('dragover', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.style.background = '#e1e7f0';
+}, false);
+
+dropZone.addEventListener('dragleave', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.style.background = '#ffffff';
+}, false);
+
+dropZone.addEventListener('drop', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.style.background = '#ffffff';
+    let files = e.dataTransfer.files;
+    if(files.length > 1) return alert('ファイルを複数投げないでね');
+    
+    loadFile(files[0]);
+    // loadModel(files[0]);
+    //loadModel('./asset/JikkenChang.vrm');
+}, false); 
 
 //シーンの準備
 const scene = new THREE.Scene();
@@ -20,29 +46,58 @@ const directionalLight = new THREE.DirectionalLight('#ffffff', 1);
 directionalLight.position.set(1,1,1);
 scene.add(directionalLight);
 
-const loader = new THREE.GLTFLoader();
+function loadFile(file) {
+    const blob = new Blob([file], {type: "application/octet-stream"});
+    const url = URL.createObjectURL(blob);
+    loadModel(url);
+}
 
-loader.load(
-    './asset/JikkenChang.vrm',
+//VRMの読み込み
+function loadModel(path) {
+    const loader = new THREE.GLTFLoader();
+
+    loader.load(
+    path,
 
     (gltf) => {
         THREE.VRM.from(gltf).then((_vrm) => {
+
+            if(scene.children.length > 1) scene.remove(scene.children[1]);
+
             vrm = _vrm;
-            scene.add(vrm.scene);
-            
+            scene.add(vrm.scene);            
             normalPose(vrm);
 
             vrm.blendShapeProxy.setValue(THREE.VRMSchema.BlendShapePresetName.Joy, 1.0);
             vrm.blendShapeProxy.update();
             
-            console.log(vrm);
-        });
-    },
+            let title = document.getElementById('title');
+            title.innerHTML = vrm.meta.title;
 
-    (progress) => console.log('loading model...', 100.0 * (progress.loaded / progress.total), '%'),
+            
+            let author = document.getElementById('author');
+            author.innerHTML = vrm.meta.author;
+
+            let load = document.getElementById('load');
+            load.innerHTML =  '読み込んだぜ!!';
+
+            isLoaded = true;
+
+            });
+        },
+
+    (progress) => {
+        let load = document.getElementById('load');
+        load.innerHTML =  '読み込み中だぜ！';
+        // load.innerHTML =  '読み込み中...' + 100.0 * (progress.loaded / progress.total) + '%';
+        // console.log('loading model...', 100.0 * (progress.loaded / progress.total), '%')
+    },
+    
     (error) => console.error(error)
 
-);
+    );
+
+}
 
 const normalPose = (vrm) => {
     const leftUpperArm = vrm.humanoid.getBoneNode(THREE.VRMSchema.HumanoidBoneName.LeftUpperArm);
@@ -51,12 +106,20 @@ const normalPose = (vrm) => {
     rightUpperArm.rotateZ(-0.8);
 }
 
+function rotateModels(vrm, speed) {
+    const hips = vrm.humanoid.getBoneNode(THREE.VRMSchema.HumanoidBoneName.Hips);
+    hips.rotateY(speed);
+}
+
 function animate() {
     let timeNow = new Date().getTime();
     if(lastTime) {
         const elapsed = timeNow - lastTime;
         angle += (90 * elapsed) / 10000.0;
     }
+    lastTime = timeNow;
+
+    if(isLoaded) rotateModels(vrm, (Math.PI/180.0));
 }
 
 function tick() {
